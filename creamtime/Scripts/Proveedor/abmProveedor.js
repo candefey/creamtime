295e26fbed8d400158d6380f7ad9e505385ac833
candefey <candelariafey@gmail.com>
2016-10-10 20:48:38 -0300

    Avances en el abm Proveedores

--- creamtime/Scripts/Proveedor/abmProveedor.js
+++ creamtime/Scripts/Proveedor/abmProveedor.js
@@ -1,4 +1,14 @@
 ﻿$(document).ready(function () {
     today = new Date();
+    dd = today.getDate();
+    mm = today.getMonth() + 1;
+    yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    var today = dd + '/' + mm + '/' + yyyy;
     document.getElementById('lbl_fecha_alta').innerHTML = today;
 });
