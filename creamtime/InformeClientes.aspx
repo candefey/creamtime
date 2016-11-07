23731d94ecf2c869909d4d72fca3a2976aac0fba
Martin Sampietro <msampietro93@gmail.com>
2016-11-07 08:57:35 -0300

    Creacion de informes

---
+++ creamtime/InformeClientes.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InformeClientes.aspx.cs" Inherits="creamtime.InformeClientes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ creamtime/InformePedidos.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace creamtime
+{
+    public partial class InformePedidos : System.Web.UI.Page
+    {
+        protected void Page_Prerender(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                if ((String)Session["user_perm"] == "Personal")
+                {
+                    this.Page_Load(sender, e);
+                }
+                else
+                {
+                    Response.Redirect("~/403Forbidden", false);
+                }
+
+            }
+            else
+            {
+                Response.Redirect("~/Login", false);
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
